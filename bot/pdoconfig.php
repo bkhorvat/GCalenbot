a62c07e1c368f1efff7383c8fd6f74bfcebd31b3
bkhorvat <bkhorvat.php@gmail.com>
2022-07-09 07:25:50 +0000

    Delete database lag, pass

--- bot/pdoconfig.php
+++ bot/pdoconfig.php
@@ -1,5 +1,5 @@
 <?php
     $host = 'localhost';
-    $dbname = 'TelegramBotCalendar';
-    $username = 'telegramBotCalendar';
-    $password = 'Dm@my14r';
+    $dbname = '';
+    $username = '';
+    $password = '';
