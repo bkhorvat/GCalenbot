b5c2193d1476b9b90fc727ed007de8a05df9bf8b
bkhorvat <bkhorvat.php@gmail.com>
2022-07-14 06:51:34 +0000

    Add Menu button

--- bot/oAuth2.0/indexoAuth.php
+++ bot/oAuth2.0/indexoAuth.php
@@ -7,6 +7,9 @@ switch ($text)
   case '/menu':
     require_once 'bot/commands/startCommand.php';
     break;
+  case 'Menu':
+    require_once 'bot/commands/startCommand.php';
+    break;
   case 'Logout':
     require_once 'bot/oAuth2.0/logoutCommand.php';
     break;
