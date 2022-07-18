6a88d9edc04f1bb77368f470e3a3f47ba56d3236
bkhorvat <bkhorvat.php@gmail.com>
2022-07-18 11:31:02 +0000

    Commit database info

--- bot/databaseconnect.php
+++ bot/databaseconnect.php
@@ -3,7 +3,7 @@ require_once 'pdoconfig.php';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    echo "Connected to $dbname at $host successfully.";
+    //echo "Connected to $dbname at $host successfully.";
 } catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 }
